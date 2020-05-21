5130732148e06946998ae64e45225e5f48131208
Vinicius Saran <viniciussaran@gmail.com>
2020-05-21 16:01:47 -0300

    testando formulário

---
+++ pages/contato.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <form name="contactus" method="post" netlify netlify-honeypot="bot-field">
+      <input type="hidden" name="form-name" value="contactus" />
+      <div class="form-group">
+        <label for="name">Name:</label>
+        <input type="text" class="form-control" name="name" required />
+      </div>
+      <div class="form-group">
+        <label for="email">Email:</label>
+        <input type="email" name="email" class="form-control" required />
+      </div>
+      <div class="form-group">
+        <label for="message">Message:</label>
+        <textarea name="message" class="form-control" required></textarea>
+      </div>
+      <button type="submit" value="Send message">Send</button>
+    </form>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from "vue-property-decorator";
+import PostItem from "@/components/PostItem.vue";
+
+@Component({
+  head() {
+    return {
+      title: "Home"
+    };
+  },
+  components: {
+    PostItem
+  }
+})
+export default class Blog extends Vue {
+  get posts() {
+    return this.$store.state.blogPosts;
+  }
+}
+</script>
+
+<style lang="scss"></style>
